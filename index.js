3fcf9c601e281f1a4a1ce8b688a490539bf15efb
Meseret <mesiteklu2018@gmail.com>
2023-11-06 22:42:34 -0700

    added codes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import express from "express.js";
-const app = express.js();
+import express from "express";
+const app = express();
 const port = 3000;
 
+app.get ("/", (req, res) => {
+  res.send("Heloo, World!");
+});
+
 app.listen(port, () => {
-  console.log('Server running on port ${port}.');
+  console.log(`Server running on port ${port}.`);
 })
